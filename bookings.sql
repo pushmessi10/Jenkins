faf7bbec4c26783a56c467aeb6a55c5baa3c6132
pushmessi10 <43080504+pushmessi10@users.noreply.github.com>
2018-11-11 21:07:29 -0600

    Update bookings.sql

--- bookings.sql
+++ bookings.sql
@@ -1,11 +1,8 @@
-CREATE TABLE IF NOT EXISTS pushkar_database.bookings (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30) null,
-Phone_no INT(12) null,
-Address VARCHAR(30) null,
-Email VARCHAR(30) null,
+CREATE TABLE bookings (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email varchar(30) null,
+Name varchar(30) null,
 DOB date null,
-Dance_type VARCHAR(30) null
+Address varchar(30) null,
+Dance Type varchar(30) null
 )ENGINE=innodb;
-
-
